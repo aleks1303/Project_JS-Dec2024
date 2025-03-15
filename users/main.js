1d25278f252444bd495b94b21c5d7367cc00862f
aleks1303 <aleksbulda13@gmail.com>
2025-03-15 21:23:40 +0200

    Project JS Dec-2024

--- users/main.js
+++ users/main.js
@@ -1,9 +1,3 @@
-// В index.html
-// 1 отримати масив об'єктів з endpoint`а https://jsonplaceholder.typicode.com/users
-// 2 Вивести id, name всіх user в index.html. Окремий блок для кожного user.
-// 3 Додати кожному блоку кнопку/посилання, при кліку на яку відбувається перехід  на сторінку user-details.html, котра має детальну інформацію про об'єкт на який клікнули
-// index.html - всі блоки з user - по 2 в рядок. кнопки/посилання розташувати під інформацією про user.
-
 
 const blockUsers = document.getElementById('users')
 
