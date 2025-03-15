157795e647f3f9263b20d8907a8bd3f40dcc83b7
aleks1303 <aleksbulda13@gmail.com>
2025-03-15 16:52:08 +0200

    Project JS Dec-2024

--- comments/post-details.js
+++ comments/post-details.js
@@ -1,13 +1,5 @@
-
-
-// На странице post-details.html:
-// 7 Вивести всю, без виключення, інформацію про об'єкт post на який клікнули .
-// 8 Нижчє інформаці про пост, вивести всі коментарі поточного поста (ендпоінт  - https://jsonplaceholder.typicode.com/posts/POST_ID/comments)
-
 const postId = localStorage.getItem('postId')
-
 const id = localStorage.getItem('id')
-
 
 const urlPost = new URL('https://jsonplaceholder.typicode.com/posts')
 urlPost.searchParams.set('id', id)
@@ -17,57 +9,48 @@ urlComments.searchParams.set('postId', postId);
 
 const infoPost = document.getElementById('post-details');
 
- async function commentsOfPost () {
+async function commentsOfPost() {
+    const divPost = document.createElement('div')
+    divPost.classList.add('infoPost');
+    function createParagraph(text) {
+        const p = document.createElement('p');
+        p.innerText = text;
+        return p;
+    }
 
-     const divPost = document.createElement('div')
-     divPost.classList.add('infoPost')
-
-     const post = await fetch(urlPost)
-         .then((response) => response.json())
-     for (const item of post) {
-         const p1 = document.createElement('p')
-         p1.innerText = `userId: ${item.userId}`;
-         const p2 = document.createElement('p')
-         p2.innerText = `id: ${item.id}`;
-         const p3 = document.createElement('p')
-         p3.innerText = `title:
-                         ${item.title}`
-         const p4 = document.createElement('p')
-         p4.innerText = `body: 
-                        ${item.body}`
-
-        divPost.append(p1, p2, p3, p4)
-     }
-
-     const title = document.createElement('h2')
-     title.innerText = `Comments`
-
-
+    const post = await fetch(urlPost)
+        .then((response) => response.json())
+    for (const item of post) {
+        divPost.append(
+            createParagraph(`userId: ${item.userId}`),
+            createParagraph(`id: ${item.id}`),
+            createParagraph(`title:
+                                    ${item.title}`),
+            createParagraph(`body: 
+                                    ${item.body}`),
+        )
+    }
+    const title = document.createElement('h2')
+    title.innerText = `Comments`
     const divComments = document.createElement('div')
-     divComments.classList.add('info')
+    divComments.classList.add('info')
     const comments = await fetch(urlComments)
         .then((response) => response.json())
-     for (const comment of comments) {
-         const commentInfo = document.createElement('div')
-         commentInfo.classList.add('comment')
-
-         const p1 = document.createElement('p')
-         p1.innerText = `userId: ${comment.postId}`;
-         const p2 = document.createElement('p')
-         p2.innerText = `id: ${comment.id}`;
-         const p3 = document.createElement('p')
-         p3.innerText = `email: 
-                        ${comment.email}`
-         const p4 = document.createElement('p')
-         p4.innerText = `name:
-                         ${comment.name}`
-         const p5 = document.createElement('p')
-         p5.innerText = `body: 
-                        ${comment.body}`
-
-        commentInfo.append(p1, p2, p3, p4, p5)
-         divComments.appendChild(commentInfo)
-     }
-     infoPost.append(divPost, title, divComments)
+    for (const comment of comments) {
+        const commentInfo = document.createElement('div')
+        commentInfo.classList.add('comment')
+        commentInfo.append(
+            createParagraph(`userId: ${comment.postId}`),
+            createParagraph(`id: ${comment.id}`),
+            createParagraph(`email:
+                                  ${comment.email}`),
+            createParagraph(`name:
+                                  ${comment.name}`),
+            createParagraph(`body:
+                                  ${comment.body}`)
+        )
+        divComments.appendChild(commentInfo)
+    }
+    infoPost.append(divPost, title, divComments)
 }
 commentsOfPost()
